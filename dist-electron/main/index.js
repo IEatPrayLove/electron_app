1c8a920472498fa08e2a6fd4f2955edec899a428
wangyu <13100684032@163.com>
2024-01-09 09:21:28 +0800

    feat first

---
+++ dist-electron/main/index.js
@@ -0,0 +1,87 @@
+"use strict";
+const electron = require("electron");
+const os = require("os");
+const path = require("path");
+const url$1 = require("url");
+var _documentCurrentScript = typeof document !== "undefined" ? document.currentScript : null;
+const __filename$1 = url$1.fileURLToPath(typeof document === "undefined" ? require("url").pathToFileURL(__filename).href : _documentCurrentScript && _documentCurrentScript.src || new URL("index.js", document.baseURI).href);
+const __dirname$1 = path.dirname(__filename$1);
+process.env.DIST_ELECTRON = path.join(__dirname$1, "../");
+process.env.DIST = path.join(process.env.DIST_ELECTRON, "../dist");
+process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL ? path.join(process.env.DIST_ELECTRON, "../public") : process.env.DIST;
+if (os.release().startsWith("6.1"))
+  electron.app.disableHardwareAcceleration();
+if (process.platform === "win32")
+  electron.app.setAppUserModelId(electron.app.getName());
+if (!electron.app.requestSingleInstanceLock()) {
+  electron.app.quit();
+  process.exit(0);
+}
+let win = null;
+const preload = path.join(__dirname$1, "../preload/index.mjs");
+const url = process.env.VITE_DEV_SERVER_URL;
+const indexHtml = path.join(process.env.DIST, "index.html");
+async function createWindow() {
+  win = new electron.BrowserWindow({
+    title: "Main window",
+    icon: path.join(process.env.VITE_PUBLIC, "favicon.ico"),
+    webPreferences: {
+      preload
+      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
+      // nodeIntegration: true,
+      // Consider using contextBridge.exposeInMainWorld
+      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
+      // contextIsolation: false,
+    }
+  });
+  if (url) {
+    win.loadURL(url);
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(indexHtml);
+  }
+  win.webContents.on("did-finish-load", () => {
+    win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
+  });
+  win.webContents.setWindowOpenHandler(({ url: url2 }) => {
+    if (url2.startsWith("https:"))
+      electron.shell.openExternal(url2);
+    return { action: "deny" };
+  });
+}
+electron.app.whenReady().then(createWindow);
+electron.app.on("window-all-closed", () => {
+  win = null;
+  if (process.platform !== "darwin")
+    electron.app.quit();
+});
+electron.app.on("second-instance", () => {
+  if (win) {
+    if (win.isMinimized())
+      win.restore();
+    win.focus();
+  }
+});
+electron.app.on("activate", () => {
+  const allWindows = electron.BrowserWindow.getAllWindows();
+  if (allWindows.length) {
+    allWindows[0].focus();
+  } else {
+    createWindow();
+  }
+});
+electron.ipcMain.handle("open-win", (_, arg) => {
+  const childWindow = new electron.BrowserWindow({
+    webPreferences: {
+      preload,
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  if (process.env.VITE_DEV_SERVER_URL) {
+    childWindow.loadURL(`${url}#${arg}`);
+  } else {
+    childWindow.loadFile(indexHtml, { hash: arg });
+  }
+});
+//# sourceMappingURL=index.js.map
